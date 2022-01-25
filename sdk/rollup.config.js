49b258a16898904c4acd62c633639db59f2355f7
Antonio Villagra De La Cruz <5470315+AntonioVdlC@users.noreply.github.com>
2022-01-25 11:20:25 +0800

    Simplify SDK

--- sdk/rollup.config.js
+++ sdk/rollup.config.js
@@ -5,38 +5,7 @@ export default [
   {
     input: "src/index.ts",
     output: {
-      file: "dist/index.cjs.js",
-      format: "cjs",
-      exports: "default",
-    },
-    plugins: [
-      typescript(),
-      terser({
-        format: {
-          comments: false,
-        },
-      }),
-    ],
-  },
-  {
-    input: "src/index.ts",
-    output: {
-      file: "dist/index.esm.js",
-      format: "es",
-    },
-    plugins: [
-      typescript(),
-      terser({
-        format: {
-          comments: false,
-        },
-      }),
-    ],
-  },
-  {
-    input: "src/index.ts",
-    output: {
-      file: "dist/index.iife.js",
+      file: "dist/index.js",
       format: "iife",
       name: "sdk", // TODO: change
     },

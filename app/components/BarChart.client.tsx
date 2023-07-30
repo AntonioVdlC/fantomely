5a6773dcb2b67f53b0c3175371f3c4443f7d13a5
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2023-07-30 18:47:44 +0800

    Update bar chart

--- app/components/BarChart.client.tsx
+++ app/components/BarChart.client.tsx
@@ -1,25 +1,56 @@
-import { BarGroup, BarSeries, XYChart } from "@visx/xychart";
-import { useState } from "react";
+import * as Plot from "@observablehq/plot";
+import { useEffect, useRef, useState } from "react";
 import type { DashboardElement } from "~/types/dashboard";
 
 type Props = {
   data: DashboardElement[];
-  dataKey: string;
   limit?: number;
 };
 
 const VIEW_LIMIT = 4;
 
-export default function BarChart({ data, dataKey, limit = VIEW_LIMIT }: Props) {
+export default function BarChart({ data, limit = VIEW_LIMIT }: Props) {
   const [viewAll, setViewAll] = useState(false);
+  const chartContainer = useRef<HTMLDivElement>(null);
 
-  const height =
-    55 * (1 + (viewAll || data.length < limit ? data.length : limit));
+  useEffect(() => {
+    if (!data.length) return;
 
-  const accessors = {
-    xAccessor: (datum: DashboardElement) => datum.count,
-    yAccessor: (datum: DashboardElement) => datum.label,
-  };
+    const max = data[0].count;
+    const plot = Plot.plot({
+      height: 55 * (1 + (viewAll || data.length < limit ? data.length : limit)),
+      x: { axis: "top", label: null },
+      y: { axis: null, inset: 6 },
+      marks: [
+        Plot.barX(viewAll ? data : data.slice(0, VIEW_LIMIT), {
+          x: "count",
+          y: "label",
+          fill: "steelblue",
+          sort: { y: "-x" },
+        }),
+        Plot.text(viewAll ? data : data.slice(0, VIEW_LIMIT), {
+          x: "count",
+          y: "label",
+          text: (d) => d.label,
+          textAnchor: "end",
+          dx: -30,
+          filter: (d) => d.count / max > 0.5,
+          fill: "white",
+        }),
+        Plot.text(viewAll ? data : data.slice(0, VIEW_LIMIT), {
+          x: "count",
+          y: "label",
+          text: (d) => d.label,
+          textAnchor: "start",
+          dx: 3,
+          filter: (d) => d.count / max <= 0.5,
+          fill: "currentColor",
+        }),
+      ],
+    });
+    chartContainer.current?.append(plot);
+    return () => plot.remove();
+  }, [data, viewAll]);
 
   if (!data.length) {
     return (
@@ -31,19 +62,7 @@ export default function BarChart({ data, dataKey, limit = VIEW_LIMIT }: Props) {
 
   return (
     <div className="relative">
-      <XYChart
-        height={height}
-        xScale={{ type: "linear" }}
-        yScale={{ type: "band" }}
-      >
-        <BarGroup>
-          <BarSeries
-            dataKey={dataKey}
-            data={viewAll ? data : data.slice(0, limit)}
-            {...accessors}
-          ></BarSeries>
-        </BarGroup>
-      </XYChart>
+      <div ref={chartContainer}></div>
       {data.length > limit ? (
         <div className="absolute right-[70px] top-[-20px] text-xs text-slate-500 hover:text-slate-400">
           <a onClick={() => setViewAll(!viewAll)}>
